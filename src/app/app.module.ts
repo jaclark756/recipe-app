1efaa6cd08cd20ed02e2b8bd7b818f145e7268d3
mabrjo <mabrjo64@gmail.com>
2021-03-11 18:56:43 -0600

    pushing to merge in components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InputRecipeComponent } from './components/input-recipe/input-recipe.component';
 import { ViewRecipesComponent } from './components/view-recipes/view-recipes.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,14 @@ import { ViewRecipesComponent } from './components/view-recipes/view-recipes.com
     ViewRecipesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+
+    RouterModule.forRoot([
+      {path: '', component: InputRecipeComponent},
+      {path: 'inputrecipe', component: InputRecipeComponent},
+      {path: 'viewrecpie', component: ViewRecipesComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

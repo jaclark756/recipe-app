2fb79bd8ea16e62eead5984342762116ea339f12
mabrjo <mabrjo64@gmail.com>
2021-03-11 19:05:25 -0600

    added getter and setter to recipe service

--- src/app/services/recipe.service.spec.ts
+++ src/app/services/recipe.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { RecipeServiceService } from './recipe-service.service';
+import { RecipeServiceService } from './recipe.service';
 
 describe('RecipeServiceService', () => {
   let service: RecipeServiceService;

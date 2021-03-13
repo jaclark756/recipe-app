de5d986aae752494ccc0fafb8e9f55b4271a1c71
jaclark7 <jaclark7@mccneb.edu>
2021-03-13 10:32:31 -0600

    added fab, header, and styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FabComponent } from './components/fab/fab.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FabComponent, SnackbarComponent],
   imports: [
     CommonModule
   ], 

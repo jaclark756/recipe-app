f5bc43939de000d9e52fe790fd9437069f44df45
rkscheer <rkscheer@mail.mccneb.edu>
2021-03-16 20:28:31 -0500

    added commented login only logic for guards

--- src/app/guards/router.guard.ts
+++ src/app/guards/router.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserServiceService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouterGuard implements CanActivate {
+
+  constructor( private userService: UserServiceService, public router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
+    const user = this.userService.activeUser;
+    // if (user) {
+    //   return true;
+    // } else {
+    //   this.router.navigate([''])
+    //   return false;
+    // }
+
+
   }
   
 }

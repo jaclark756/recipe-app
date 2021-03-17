30a7423dcda07f88472a13db04314a67eb6f9cf2
Jackiebeaugard <jacquelynbeaugard@gmail.com>
2021-03-16 19:28:01 -0500

    added recipe card tile component to shared folder

---
+++ src/app/shared/recipe-card-tile/recipe-card-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-card-tile',
+  templateUrl: './recipe-card-tile.component.html',
+  styleUrls: ['./recipe-card-tile.component.scss']
+})
+export class RecipeCardTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

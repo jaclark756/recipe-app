05047a3a232368ea71650830e6388304d2abefe6
jaclark7 <jaclark7@mccneb.edu>
2021-03-21 20:40:03 -0500

    removed create fab on login and signup, added profile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { InputRecipeComponent } from './components/input-recipe/input-recipe.component';
 import { ViewRecipesComponent } from './components/view-recipes/view-recipes.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: WelcomeComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'inputrecipe', component: InputRecipeComponent },
   { path: 'viewrecipe/:id', component: ViewRecipesComponent }
 ];

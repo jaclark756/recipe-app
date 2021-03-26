52724c7fd22815ea98638594397ae5bd88dc2e10
Jackiebeaugard <jacquelynbeaugard@gmail.com>
2021-03-25 21:59:31 -0500

    started edit profile tab

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {

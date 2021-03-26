c4a547c61b88a43d7eaa662cc7676eb17ce8f470
Cale Cortney <thortney@mailbox.org>
2021-03-26 10:25:22 -0500

    changed styling to the login dialog, added conditional error message

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SnackbarService } from 'src/app/shared/services/snackbar.service';
 
 @Component({
@@ -12,14 +13,21 @@ export class LoginComponent implements OnInit {
   REDIRECT: string = "?redirect_uri=http://localhost:4200/login";
   githubURI: string = this.API_BASE + "github" + this.REDIRECT;
   googleURI: string = this.API_BASE + "google" + this.REDIRECT;
+  error: string;
   
-  testFunction(event){
-    alert("TEST WORKED!");
+  googleLogin(){
+    window.location.href = this.googleURI;
+  }
+
+  githubLogin(){
+    window.location.href = this.githubURI;
   }
 
 
-  constructor( ) { }
+  constructor( @Inject(MAT_DIALOG_DATA) public data: any ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.error = this.data ? this.data.error : null;
+   }
 
 }

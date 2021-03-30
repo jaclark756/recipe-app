8856a3bc09ceade7eaaec8151114a4cb605241ee
Jackiebeaugard <jacquelynbeaugard@gmail.com>
2021-03-30 16:48:31 -0500

    began adding button functionality and error messages

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/types/user';
 
@@ -15,23 +15,35 @@ export class SettingsComponent implements OnInit {
   followersChecked = true;
   likesChecked = true;
   recipeChecked = true;
-  profileSettings: FormGroup;
+  editProfile: FormGroup;
   
   constructor(
     public fb: FormBuilder,
     public userService: UserService) {
       userService.getCurrentUser().subscribe((user: any) => this.activeUser = user);
-      this.profileSettings = this.fb.group({
-        displayName: ['', Validators.required]
+      this.editProfile = this.fb.group({
+        displayname: new FormControl('Test Name', [Validators.required, Validators.minLength(3), Validators.maxLength(50)])
       })
      }
 
   ngOnInit(): void {
   }
 
-  saveProfileInfo() {
-    
+  updateProfile() {
+    if (this.editProfile.valid && this.editProfile.dirty) {
+
+      // add functionality to update new display name
+
+      alert("Your changes have been saved!")
+    }
+    else {
+      alert("ERROR: There are no changes to be saved");
+    }
   }
+
+  revertChanges() {
+    this.editProfile.reset();  //not working.....
+  } 
 
   notifySave() {
     let object = {

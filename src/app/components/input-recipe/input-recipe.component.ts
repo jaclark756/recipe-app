b83f79488a279e34ad82772e812c6388bdd082a4
James Wiesenthal <jameswiesenthal@mail.mccneb.edu>
2021-03-30 21:18:28 -0500

    added functional category input with chips

--- src/app/components/input-recipe/input-recipe.component.ts
+++ src/app/components/input-recipe/input-recipe.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { TokenService } from 'src/app/services/token.service';
 import { UserService } from 'src/app/services/user.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/types/ingredient';
+import { Instruction } from 'src/app/types/instruction';
+import { Category } from 'src/app/types/category';
+import { Observable } from 'rxjs';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { startWith, map } from 'rxjs/operators';
+import { MatChipInputEvent } from '@angular/material/chips';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import { isObject } from 'util';
+
 
 @Component({
   selector: 'app-input-recipe',
@@ -11,9 +21,22 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class InputRecipeComponent implements OnInit {
 
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  categoryControl = new FormControl();
   newRecipe: FormGroup;
   // instructions: FormArray;
   currentUser: any;
+  Ingredients: Ingredient[];
+  Instructions: Instruction[];
+  categories: Category[] = [{'name': 'Breakfast'}, {'name': 'Gluten Free'}];
+  allCategories: Category[] = [{'name': 'Lunch'}, {'name': 'Dinner'}, {'name': 'Dessert'}];
+  filteredCategories: Observable<Category[]>;
+
+  @ViewChild('categoryInput') categoryInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
   constructor(
     public recipeService: RecipeService,
@@ -37,6 +60,9 @@ export class InputRecipeComponent implements OnInit {
       cookTime: new FormControl('', [Validators.required, Validators.min(0)]),
       prepTime: new FormControl('', [Validators.required, Validators.min(0)])
     })
+    this.filteredCategories = this.categoryControl.valueChanges.pipe(
+      startWith(null),
+      map((category: string | null) => category ? this._filter(category) : this.allCategories.slice()));
   }
 
   ngOnInit(): void {
@@ -67,6 +93,39 @@ export class InputRecipeComponent implements OnInit {
     }
     removeInstruction(event) {
       // form array loop has index, need to have button next to form field that passes in the index from loop and uses it to remove form control from the form array
+    }
+
+    add(event: MatChipInputEvent): void {
+      const input = event.input;
+      const value = event.value;
+  
+      // Add our fruit
+      if ((value || '').trim()) {
+        this.categories.push({name: value.trim()});
+      }
+        // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.categoryControl.setValue(null);
+    }
+
+    remove(categoryName: Category): void {
+      this.categories = this.categories.filter(category => categoryName.name !== category.name);
+    }
+
+    selected(event: MatAutocompleteSelectedEvent): void {
+      this.categories.push({name: event.option.viewValue});
+
+      this.categoryControl.setValue(null);
+    }
+
+    private _filter(value: string | Object): Category[] {
+      if (value && value instanceof String) {
+        const filterValue = (<string>value).toLowerCase();
+        return this.allCategories.filter(category => category.name.toLowerCase().indexOf(filterValue) === 0);
+      }
     }
   
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -25,6 +25,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { SearchDialogComponent } from './components/search-dialog/search-dialog.component';
 import { CollectionCardComponent } from './components/collection-card/collection-card.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 
 
@@ -55,7 +56,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     FlexLayoutModule,
     MatDialogModule,
     MatInputModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatAutocompleteModule
   ],
   exports: [
     HeaderComponent,
@@ -80,7 +82,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatInputModule,
     FlexLayoutModule,
     CollectionCardComponent,
-    MatTooltipModule
+    MatTooltipModule,
+    MatAutocompleteModule
   ],
   providers: [
     SnackbarService

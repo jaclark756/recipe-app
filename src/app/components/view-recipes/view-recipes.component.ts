dcf7bc8345bd6f42e5ab85fe2071f3be7fdb8d15
Ian Gurney <ianclaytongurney@gmail.com>
2021-04-13 19:39:14 -0500

    Revert "Revert "updated new-recipe button styling on profile, merged develop into 288""
    This reverts commit e2c4f4940c65527cfcae891b17ceaa6b60789f91.

--- src/app/components/view-recipes/view-recipes.component.ts
+++ src/app/components/view-recipes/view-recipes.component.ts
@@ -9,61 +9,62 @@ import { Recipe } from 'src/app/types/recipe';
 @Component({
   selector: 'app-view-recipes',
   templateUrl: './view-recipes.component.html',
-  styleUrls: ['./view-recipes.component.scss']
+  styleUrls: ['./view-recipes.component.scss'],
 })
 export class ViewRecipesComponent implements OnInit {
-
   public recipe: Recipe;
   public ingredients: string[];
   public instructions: string[];
   sample_recipes = RECIPES;
   ingredientList = [
-    "Asparagus",
+    'Asparagus',
     "Jordan's Latest eyeroll",
     "Jackie's Coolness",
     "Ryan's Tears",
-    "1 Cup Sugar",
-    "Cheezits",
+    '1 Cup Sugar',
+    'Cheezits',
     "Cale's 'u w0nt' quote",
-    "Ravioli",
-    "Crabgrass",
-    "Beetles",
-    "1/2 cup dumpster juice",
-    "A human hand",
-    "Fetta Cheese",
-    "The J&J Covid Vaccine"
-  ]
+    'Ravioli',
+    'Crabgrass',
+    'Beetles',
+    '1/2 cup dumpster juice',
+    'A human hand',
+    'Fetta Cheese',
+    'The J&J Covid Vaccine',
+  ];
 
   instructionList = [
-    "1. Preheat oven to 700 degrees Kelvin",
-    "2. Turn on centrifuge at 400rpm",
-    "3. Slam 3 Redbulls",
-    "4. Celebrate with a Commit",
-    "5. Mix all ingredients in a kiddie pool with 17 gallons of water",
-    "6. Use paverbase tamper to mash everything into small chunks",
-    "7. Simmer on stove for 20 min",
-    "8. Let cool for 5 min off stove",
-    "9. Serve with Parsely garnish",
-  ]
+    '1. Preheat oven to 700 degrees Kelvin',
+    '2. Turn on centrifuge at 400rpm',
+    '3. Slam 3 Redbulls',
+    '4. Celebrate with a Commit',
+    '5. Mix all ingredients in a kiddie pool with 17 gallons of water',
+    '6. Use paverbase tamper to mash everything into small chunks',
+    '7. Simmer on stove for 20 min',
+    '8. Let cool for 5 min off stove',
+    '9. Serve with Parsely garnish',
+  ];
 
-  constructor(private route: ActivatedRoute,
-    private recipeService: RecipeService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
     this.getRecipe();
   }
 
   getRecipe(): void {
-    this.route.paramMap.subscribe(param => {
-      this.recipeService.getRecipe(+param.get('id')).subscribe(recipe => {
+    this.route.paramMap.subscribe((param) => {
+      this.recipeService.getRecipe(+param.get('id')).subscribe((recipe) => {
         this.recipe = recipe;
-        this.ingredients = recipe.ingredients
-        this.instructions = recipe.instructions
+        this.ingredients = recipe.ingredients;
+        this.instructions = recipe.instructions;
       });
-    })
+    });
   }
 
   strikethroughText(event) {
-    event.target.classList.toggle("instructions-Strikethrough")
+    event.target.classList.toggle('instructions-Strikethrough');
   }
 }

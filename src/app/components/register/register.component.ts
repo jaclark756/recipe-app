6f80e215f54bfc74fbc78dd33b6afd7fc1064631
rkscheer <rkscheer@mail.mccneb.edu>
2021-04-17 09:07:55 -0500

    removed alert

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
+import { Token } from '@angular/compiler/src/ml_parser/lexer';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/services/token.service';
 import { ValidationService } from 'src/app/services/validation.service';
 import { environment } from 'src/environments/environment';
 
@@ -15,7 +17,7 @@ export class RegisterComponent implements OnInit {
 
   BASE_URL: string = "http://localhost:8080/"
   USERS_URL: string = "/v2/usernames"
-  SIGNUP_URL: string = "/v2/user/update"
+  SIGNUP_URL: string = "/v2/user"
   URL = this.BASE_URL + this.USERS_URL;
   UPDATE_URL = environment.apiUrl + this.SIGNUP_URL;
 
@@ -25,7 +27,8 @@ export class RegisterComponent implements OnInit {
     private http: HttpClient,
     public fb: FormBuilder,
     public validationService: ValidationService,
-    public router: Router
+    public router: Router,
+    public tokenService: TokenService,
   ) {
     this.signUpForm = fb.group({
       username: new FormControl('', [Validators.required, Validators.minLength(4)], this.validationService.userNameValidator.bind(this.validationService))
@@ -36,9 +39,8 @@ export class RegisterComponent implements OnInit {
   }
 
   signUp(event){
-    alert("signed up!");
-    console.log(this.UPDATE_URL);
-    console.log(this.signUpForm.value);
+    let user = this.tokenService.getUser();
+    console.log(user);
     this.http.put(this.UPDATE_URL, this.signUpForm.value).subscribe(
       res => {
         console.log(res)

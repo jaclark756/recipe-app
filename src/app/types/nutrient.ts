a1d3ff075d20a208e80b10efc7fe492cd14cf43b
Calvin <cjs21296@gmail.com>
2021-04-22 21:18:52 -0500

    formatted again

--- src/app/types/nutrient.ts
+++ src/app/types/nutrient.ts
@@ -1,7 +1,7 @@
 export interface Nutrient {
-    id:number;
-    name:string;
-    amount:number;
-    unit:string;
-    percentOfDailyNeeds:number;
+    id: number;
+    name: string;
+    amount: number;
+    unit: string;
+    percentOfDailyNeeds: number;
 }

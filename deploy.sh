e1a18682765e18a86435d5a28b86f702f0e34859
Eric Reichwaldt <midgetracer@gmail.com>
2021-04-29 21:37:13 -0500

    syntax updates

--- deploy.sh
+++ deploy.sh
@@ -5,28 +5,28 @@ CONTAINER_IMAGE=$3
 CONTAINER_NAME="recipe-app-${ENVIRONMENT}"
 
 if [[ ${ENVIRONMENT} == develop ]]
-        then
-                PORT=8087
-        elif [[ ${ENVIRONMENT} == master ]]
-        then
-                PORT=8081
-        else
-                PORT=9999
+    then
+        PORT=8087
+elif [[ ${ENVIRONMENT} == master ]]
+    then
+        PORT=8081
+else
+    PORT=9999
 fi
 
 if [[ PORT != 9999 ]]
-        then
-                if [[ $(docker container ls -q --filter name=${CONTAINER_NAME}) ]];
-                        then
-                                echo "found running container, killing thing"
-                                docker stop ${CONTAINER_NAME}
-                        else
-                                echo "no running container, starting new one"
-                fi
-                aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${CONTAINER_REGISTRY}
-                docker pull ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}
+    then
+        if [[ $(docker container ls -q --filter name=${CONTAINER_NAME}) ]];
+            then
+                echo "found running container, killing thing"
+                docker stop ${CONTAINER_NAME}
+        else
+            echo "no running container, starting new one"
+        fi
+        aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${CONTAINER_REGISTRY}
+        docker pull ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}
 
-                docker run -p ${PORT}:80 --rm --name ${CONTAINER_NAME} --detach ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}
-                else
-                        echo "project not built on develop or master, not deploying docker"
+        docker run -p ${PORT}:80 --rm --name ${CONTAINER_NAME} --detach ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}
+else
+        echo "project not built on develop or master, not deploying docker"
 fi

37c32ed377c58df552faee98a2ccd83c382012d9
Jackiebeaugard <jacquelynbeaugard@gmail.com>
2021-05-08 11:41:38 -0500

    adding sample data

--- src/app/shared/components/collection/collection.component.ts
+++ src/app/shared/components/collection/collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RECIPES } from 'src/app/helpers/sample-data';
 
 @Component({
   selector: 'app-collection',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionComponent implements OnInit {
 
+  sample_recipes = RECIPES;
   constructor() { }
 
   ngOnInit(): void {

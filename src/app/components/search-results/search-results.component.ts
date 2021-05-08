45a88e66dbd1ac13746c1a95c91b6bc017fc0b87
James Wiesenthal <jameswiesenthal@mail.mccneb.edu>
2021-05-08 12:21:24 -0500

    fixed commented out stuff and console log and unused code

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -19,7 +19,6 @@ import { Recipe } from 'src/app/types/recipe';
       private _searchService: SearchService,
       private route: ActivatedRoute,
       ) {
-      console.log("this is loading")
      }
   
     ngOnInit(): void {
@@ -30,16 +29,10 @@ import { Recipe } from 'src/app/types/recipe';
       this.route.queryParamMap.subscribe(param => {
         param.keys.forEach((k) => {
           this.httpParams = this.httpParams.set(k, param.get(k));
-          // console.log("httpParams: ", this.httpParams.toString())
         });
         this._searchService.getSearchedRecipes(this.httpParams).subscribe(response => {
           this.recipes = response;
-          console.log("recipes: ", this.recipes)
         })
-        
-        // pipe(map(response => {
-        //   this.recipes = response;
-        // }));
       })
     }
   }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -16,20 +16,6 @@ export class SearchService {
 
     userSearchValue$: Observable<any>;
     private url: string = environment.apiUrl + "/v2/recipe"
-    private httpParams: HttpParams;
-
-    buildUrl(url: string, ... params: any[]){
-        if (params.length > 0){
-          let str = url + "?"; 
-          params.forEach((s) => {
-            if (typeof(s) === "object"){
-              str += s.k + "=" + s.v + "&"
-            }
-          })
-          return str.slice(0, -1)
-        } 
-        return url
-      }
 
     constructor(
         private http: HttpClient,

2329fb9301c93b14fdba3d9f5b2883070b09283c
Cale Cortney <thortney@mailbox.org>
2021-05-18 17:26:44 -0500

    cleaned up username form validator

--- src/app/services/validation.service.ts
+++ src/app/services/validation.service.ts
@@ -11,25 +11,24 @@ export class ValidationService {
   BASE_URL: string = environment.apiUrl
   USERS_URL: string = "/api/v2/usernames"
   URL = this.BASE_URL + this.USERS_URL;
+  usernames: any[]
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {this.loadUsernames()}
 
-  checkUsername(){
-    this.http.get(this.URL).subscribe
+  loadUsernames(){
+    this.http.get(this.URL).subscribe((s: string[]) => {
+      this.usernames = s
+    })
   }
+
 
   userNameValidator(userControl: AbstractControl) { 
     return new Promise(resolve => {  
-      setTimeout(() => {  
-
-        this.http.get(this.URL).subscribe((s: string[]) => {
-          if (s.includes(userControl.value)){
-            resolve({taken: true});
-          } else {
-            resolve(null);
-          }
-        })
-      }, 300);  
+      if (this.usernames.includes(userControl.value)){
+        resolve({taken: true});
+      } else {
+        resolve(null);
+      }
     });  
   }  
 

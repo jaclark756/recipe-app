a1e3025123e1b4f5228468a21d7965e8f63213a8
rkscheer <rkscheer@mail.mccneb.edu>
2021-05-18 19:38:06 -0500

    updated viewrecipe to include notes

--- src/app/components/view-recipes/view-recipes.component.ts
+++ src/app/components/view-recipes/view-recipes.component.ts
@@ -12,6 +12,7 @@ import { NutrientEntity } from 'src/app/types/NutrientEntity';
 import { Recipe } from 'src/app/types/recipe';
 import { CollectionDialogComponent } from 'src/app/shared/components/collection-dialog/collection-dialog.component';
 import { InputRecipeComponent } from '../input-recipe/input-recipe.component';
+import { RecipeUpdateNote } from 'src/app/types/recipeUpdateNote';
 
 @Component({
   selector: 'app-view-recipes',
@@ -22,6 +23,7 @@ export class ViewRecipesComponent implements OnInit {
   public recipe: Recipe;
   public ingredients: Ingredient[];
   public instructions: Instruction[];
+  public notes: RecipeUpdateNote[] = [];
   public relatedRecipes: any;
   sample_recipes = RECIPES;
   ingredientList = [
@@ -73,8 +75,8 @@ export class ViewRecipesComponent implements OnInit {
           this.relatedRecipes = this.recipeService.findRelatedRecipes(recipe);
           this.ingredients = recipe.ingredients;
           this.instructions = recipe.instructions;
+          this.notes = recipe.notes;
         })
-        
       };
     })
   }
